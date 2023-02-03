282d516875c23938915988ad132842344d8e7350
Saragam Ghimire <codewithghimire@gmail.com>
2023-02-03 20:16:11 +0545

    using request and response

--- src/customers/services/customers/customers.service.ts
+++ src/customers/services/customers/customers.service.ts
@@ -2,11 +2,23 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class CustomersService {
-    findCustomer(){
-        return {
-            email:'test@admin.com',
+    users = [
+        {
             id:1,
-            createdAt: new Date()
-        };
+            email:'admin@test.com',
+            createdAt:new Date()
+        },{
+            id:2,
+            email:'superadmin@test.com',
+            createdAt:new Date()
+        },{
+            id:3,
+            email:'user@test.com',
+            createdAt:new Date()
+        }
+    ];
+
+    findCustomerById(id:number){
+        return this.users.find((user)=>user.id ===id)
     }
 }
