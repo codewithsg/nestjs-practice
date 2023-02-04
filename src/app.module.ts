c2add5e00fe59a8ad19f4505f247ca0d2c4a42fc
Saragam Ghimire <codewithghimire@gmail.com>
2023-02-04 18:45:18 +0545

    connected to mysql and created an entity named User with columns id, username, email and password

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CustomersModule } from './customers/customers.module';
+import entities from './typeORM';
+import { User } from './typeORM/User';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [CustomersModule, UsersModule],
+  imports: [
+    CustomersModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type:'mysql',
+      host:'localhost',
+      port:3306,
+      username:'root',
+      password:'root',
+      database:'nestjs_practice_db',
+      entities:entities,
+      synchronize:true 
+    })
+  ],
   controllers: [],
   providers: [],
 })
